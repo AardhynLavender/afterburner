d11dc58b6abad9da9c89cd0d36bc98314d760053
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-08-16 10:40:12 +1200

    chore: simplify static

--- static.ts
+++ static.ts
@@ -1,175 +1,85 @@
 // todo: store these in a bucket somewhere
 
 export const chapterOrderings = [
-  [
-    "Track-1",
-    //
-    "Track-2A",
-    "Track-2B",
-    "Track-3",
-    "Track-4",
-    "Track-5A",
-    "Track-5B",
-    "Track-6",
-    "Track-7",
-    //
-    "Track-8",
-    "Track-9",
-    "Track-9A",
-  ],
-  [
-    "Track-1",
-    //
-    "Track-3",
-    "Track-6",
-    "Track-7",
-    "Track-4",
-    "Track-5A",
-    "Track-5B",
-    "Track-2A",
-    "Track-2B",
-    //
-    "Track-8",
-    "Track-9",
-    "Track-9A",
-  ],
-  [
-    "Track-1",
-    //
-    "Track-6",
-    "Track-4",
-    "Track-2A",
-    "Track-2B",
-    "Track-3",
-    "Track-5A",
-    "Track-5B",
-    "Track-7",
-    //
-    "Track-8",
-    "Track-9",
-    "Track-9A",
-  ],
-  [
-    "Track-1",
-    //
-    "Track-5A",
-    "Track-5B",
-    "Track-4",
-    "Track-2A",
-    "Track-2B",
-    "Track-3",
-    "Track-7",
-    "Track-6",
-    //
-    "Track-8",
-    "Track-9",
-    "Track-9A",
-  ],
-  [
-    "Track-1",
-    //
-    "Track-7",
-    "Track-3",
-    "Track-2A",
-    "Track-2B",
-    "Track-5A",
-    "Track-5B",
-    "Track-4",
-    "Track-6",
-    //
-    "Track-8",
-    "Track-9",
-    "Track-9A",
-  ],
-  [
-    "Track-1",
-    //
-    "Track-4",
-    "Track-7",
-    "Track-6",
-    "Track-2A",
-    "Track-2B",
-    "Track-3",
-    "Track-5A",
-    "Track-5B",
-    //
-    "Track-8",
-    "Track-9",
-    "Track-9A",
-  ],
+  [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11],
+  [0, 3, 7, 8, 4, 5, 6, 1, 2, 9, 10, 11],
+  [0, 7, 4, 1, 2, 3, 5, 6, 8, 9, 10, 11],
+  [0, 5, 6, 4, 1, 2, 3, 8, 7, 9, 10, 11],
+  [0, 8, 3, 1, 2, 5, 6, 4, 7, 9, 10, 11],
+  [0, 4, 8, 7, 1, 2, 3, 5, 6, 9, 10, 11],
 ];
 
 export const chapters = [
   {
-    id: "1",
     title: "The Source",
     description: "The Source",
+    file_name: "Track-1",
     audio_file: require("./assets/audio/Track-1.mp3"),
   },
   {
-    id: "2",
     title: "Station 1: Time transportation station",
     description: "find the area with two hanging black and white images",
+    file_name: "Track-2A",
     audio_file: require("./assets/audio/Track-2A.mp3"),
   },
   {
-    id: "3",
     title: "Station 1: Time transportation station (Part 2)",
     description: "View the photos",
+    file_name: "Track-2B",
     audio_file: require("./assets/audio/Track-2B.mp3"),
   },
   {
-    id: "4",
     title: "Station 2: Schrodinger's cat",
     description: "find the area with the cat",
+    file_name: "Track-3",
     audio_file: require("./assets/audio/Track-3.mp3"),
   },
   {
-    id: "5",
     title: "Station 3: Observe the Observers/The Power of Attention",
     description: "walk around the space",
+    file_name: "Track-4",
     audio_file: require("./assets/audio/Track-4.mp3"),
   },
   {
-    id: "6",
     title: "Station 4: Duality Station",
     description: "find the box of duality",
+    file_name: "Track-5A",
     audio_file: require("./assets/audio/Track-5A.mp3"),
   },
   {
-    id: "7",
     title: "Station 4: Duality Station (Part 2)",
     description: "Write the words",
+    file_name: "Track-5B",
     audio_file: require("./assets/audio/Track-5B.mp3"),
   },
   {
-    id: "8",
     title: "Station 5: The Dice Ladder",
     description: "find the dice ladder",
+    file_name: "Track-6",
     audio_file: require("./assets/audio/Track-6.mp3"),
   },
   {
-    id: "9",
     title: "Station 6: We're all in this Together",
     description: "Notice the space around you",
+    file_name: "Track-7",
     audio_file: require("./assets/audio/Track-7.mp3"),
   },
   {
-    id: "10",
     title: "Station 7: The Return Home",
     description: "colorful circles!!!",
+    file_name: "Track-8",
     audio_file: require("./assets/audio/Track-8.mp3"),
   },
   {
-    id: "11",
     title: "Station 7: The Return Home (Part 2)",
     description: "jump from circle to circle",
+    file_name: "Track-9",
     audio_file: require("./assets/audio/Track-9.mp3"),
   },
   {
-    id: "12",
     title: "Station 7: The Return Home (Part 3)",
     description: "Back to the source",
+    file_name: "Track-9A",
     audio_file: require("./assets/audio/Track-9A.mp3"),
   },
 ];
