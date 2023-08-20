3ae4f8b82ec344f2900b413cc0fbee93f5f043d8
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-08-21 11:48:11 +1200

    feat: interaction boilerplate

--- screens/shows/show/Show.tsx
+++ screens/shows/show/Show.tsx
@@ -1,10 +1,11 @@
-import { useState } from "react";
+import { useMemo, useState } from "react";
 import { View, Text, StyleSheet, Button } from "react-native";
 import { invariant } from "../../../exception/invariant";
 import { ShowScreenProps } from "../../../navigation";
 import useSound from "../../../sound/sound";
 import { usePersistent } from "../../../api/persistent";
 import { Chapter, chapters } from "../../../static";
+import type { Interaction } from "../../../static";
 import { START_MS } from "../../../sound/sound";
 import React from "react";
 
@@ -49,9 +50,6 @@ function CurrentChapter({
   onNext: () => void;
 }) {
   const [position, setPosition] = useState(0);
-  const handleTick = (position: number) => {
-    setPosition((position / duration) * 100);
-  };
 
   const handleNext = () => {
     setPosition(START_MS);
@@ -60,7 +58,7 @@ function CurrentChapter({
 
   const { isLoaded, playing, error, play, pause, duration } = useSound(
     chapter.audio_file,
-    { autoPlay: true, onTick: handleTick, onFinish: handleNext }
+    { autoPlay: true, onTick: setPosition }
   );
 
   if (!isLoaded) return <Text>Loading...</Text>;
@@ -72,13 +70,20 @@ function CurrentChapter({
 
   return (
     <View style={styles.container}>
-      <Text>{chapter.title}</Text>
+      <Text style={styles.chapterHeading}>{chapter.title}</Text>
       <View style={styles.progressBar}>
-        <View style={[styles.progress, { width: `${position}%` }]} />
+        <View
+          style={[
+            styles.progress,
+            { width: `${(position / duration) * 100}%` },
+          ]}
+        />
       </View>
-      <View style={styles.interaction}>
-        <Text>interaction goes here...</Text>
-      </View>
+      <Interaction
+        interactions={chapter.interactions}
+        position={position}
+        duration={duration}
+      />
       <View style={styles.buttons}>
         <Button
           title={playing ? "Pause" : "Resume"}
@@ -99,12 +104,94 @@ function StartShow({ onStart: handleStart }: { onStart: () => void }) {
   );
 }
 
+function Interaction({
+  interactions,
+  position,
+  duration,
+}: {
+  interactions?: Interaction[];
+  position: number;
+  duration: number;
+}) {
+  const activeInteractions = useMemo(
+    () =>
+      interactions?.filter((interaction) =>
+        displayInteraction(
+          interaction.start_timestamp,
+          interaction.end_timestamp,
+          position,
+          duration
+        )
+      ),
+    [interactions, position, duration]
+  );
+
+  return (
+    <View style={styles.interaction}>
+      <Text>Interaction</Text>
+      {activeInteractions?.map((interaction, key) => {
+        switch (interaction.type) {
+          case "next":
+            return <NextChapterInteraction key={key} />;
+          case "hero":
+            return <Hero key={key} />;
+          default:
+            throw new Error(`Unknown interaction type: ${interaction}`);
+        }
+      })}
+    </View>
+  );
+}
+
+function NextChapterInteraction() {
+  return (
+    <View>
+      <Text>Next Chapter</Text>
+      <Button title="Next" onPress={() => {}} />
+    </View>
+  );
+}
+
+function Hero() {
+  return (
+    <View>
+      <Text>Find a thing</Text>
+    </View>
+  );
+}
+
+const DEFAULT_START_TIMESTAMP = 0;
+function displayInteraction(
+  startTimestamp: number | undefined,
+  endTimestamp: number | undefined,
+  position: number,
+  duration: number
+) {
+  const startRelative = startTimestamp ?? DEFAULT_START_TIMESTAMP;
+  const endRelative = endTimestamp ?? duration;
+
+  // negative timestamps are relative to the end of the track
+  const start = startRelative < 0 ? duration + startRelative : startRelative;
+  const end = endRelative < 0 ? duration - endRelative : endRelative;
+
+  if (start < 0 || start > duration)
+    throw new Error(`Invalid start timestamp: ${start}`);
+  if (end < 0 || end > duration)
+    throw new Error(`Invalid end timestamp: ${end}`);
+
+  return start <= position && position <= end;
+}
+
 const styles = StyleSheet.create({
   container: {
     alignItems: "center",
     margin: 16,
     flex: 1,
     gap: 16,
+  },
+  chapterHeading: {
+    fontSize: 24,
+    fontWeight: "bold",
   },
   progressBar: {
     height: 8,
--- static.ts
+++ static.ts
@@ -1,3 +1,4 @@
+import { AVPlaybackSource } from "expo-av";
 // todo: store these in a bucket somewhere
 
 export const chapterOrderings = [
@@ -9,77 +10,178 @@ export const chapterOrderings = [
   [0, 4, 8, 7, 1, 2, 3, 5, 6, 9, 10, 11],
 ];
 
-export const chapters = [
+export type Chapter = {
+  title: string;
+  description: string;
+  file_name: string;
+  audio_file: AVPlaybackSource;
+  show_next_after?: number; // milliseconds thought to show next button
+  interactions?: Interaction[];
+};
+
+export type InteractionType = "hero" | "image" | "video" | "next";
+export type InteractionPrimitive = {
+  type: InteractionType;
+  start_timestamp?: number; // undefined indicates the start
+  end_timestamp?: number; // undefined indicates the end
+};
+
+export type NextInteraction = InteractionPrimitive & { type: "next" };
+export type HeroInteraction = InteractionPrimitive & {
+  type: "hero";
+  text: string;
+};
+
+export type Interaction = NextInteraction | HeroInteraction;
+
+export const chapters: Chapter[] = [
   {
     title: "The Source",
     description: "The Source",
     file_name: "Track-1",
     audio_file: require("./assets/audio/Track-1.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 1: Time transportation station",
     description: "find the area with two hanging black and white images",
     file_name: "Track-2A",
     audio_file: require("./assets/audio/Track-2A.mp3"),
+    interactions: [
+      {
+        type: "hero",
+        end_timestamp: 6000,
+        text: "Find an area with two hanging hand black and white photos",
+      },
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 1: Time transportation station (Part 2)",
     description: "View the photos",
     file_name: "Track-2B",
     audio_file: require("./assets/audio/Track-2B.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 2: Schrodinger's cat",
     description: "find the area with the cat",
     file_name: "Track-3",
     audio_file: require("./assets/audio/Track-3.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 3: Observe the Observers/The Power of Attention",
     description: "walk around the space",
     file_name: "Track-4",
     audio_file: require("./assets/audio/Track-4.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 4: Duality Station",
     description: "find the box of duality",
     file_name: "Track-5A",
     audio_file: require("./assets/audio/Track-5A.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 4: Duality Station (Part 2)",
     description: "Write the words",
     file_name: "Track-5B",
     audio_file: require("./assets/audio/Track-5B.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 5: The Dice Ladder",
     description: "find the dice ladder",
     file_name: "Track-6",
     audio_file: require("./assets/audio/Track-6.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 6: We're all in this Together",
     description: "Notice the space around you",
     file_name: "Track-7",
     audio_file: require("./assets/audio/Track-7.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 7: The Return Home",
     description: "colorful circles!!!",
     file_name: "Track-8",
     audio_file: require("./assets/audio/Track-8.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 7: The Return Home (Part 2)",
     description: "jump from circle to circle",
     file_name: "Track-9",
     audio_file: require("./assets/audio/Track-9.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
   {
     title: "Station 7: The Return Home (Part 3)",
     description: "Back to the source",
     file_name: "Track-9A",
     audio_file: require("./assets/audio/Track-9A.mp3"),
+    interactions: [
+      {
+        type: "next",
+        start_timestamp: -1000,
+      },
+    ],
   },
 ];
