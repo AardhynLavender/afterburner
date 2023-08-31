4b50bd550d291622acec51d6015d6ea125bb89b6
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-08-31 20:04:59 +1200

    chore: top level improvements

--- App.tsx
+++ App.tsx
@@ -1,19 +1,11 @@
-import { NavigationContainer } from "@react-navigation/native";
 import React, { useState } from "react";
-import Fi from "react-native-vector-icons/Feather";
-import { Home, Memories, Scan, Shows, Settings } from "./screens";
-import { RootTabNavigator, RootTabScreen } from "./navigation";
-import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
 import { loadAsync } from "expo-font";
 import { useEffect } from "react";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { registerRootComponent } from "expo";
 import { SafeAreaProvider } from "react-native-safe-area-view";
 import AuthProvider from "./contexts/auth";
-
-const stdProps: BottomTabNavigationOptions = {
-  tabBarShowLabel: false,
-};
+import Layout from "./Layout";
 
 async function load() {
   await loadAsync({
@@ -40,66 +32,7 @@ export default function App() {
     <SafeAreaProvider>
       <QueryClientProvider client={queryClient}>
         <AuthProvider>
-          <NavigationContainer>
-            <RootTabNavigator initialRouteName="scan">
-              {/* <RootTabScreen
-              name="home"
-              component={Home}
-              options={{
-                title: "Home",
-                tabBarIcon: ({ color, size }) => (
-                  <Fi name="home" size={size} color={color} />
-                ),
-                ...stdProps,
-              }}
-            /> */}
-              <RootTabScreen
-                name="shows"
-                component={Shows}
-                options={{
-                  title: "Shows",
-                  tabBarIcon: ({ color, size }) => (
-                    <Fi name="info" size={size} color={color} />
-                  ),
-                  ...stdProps,
-                }}
-              />
-              <RootTabScreen
-                name="scan"
-                component={Scan}
-                options={{
-                  title: "Scan Ticket",
-                  unmountOnBlur: true, // unmount camera when not in view
-                  tabBarIcon: ({ color, size }) => (
-                    <Fi name="camera" size={size} color={color} />
-                  ),
-                  ...stdProps,
-                }}
-              />
-              {/* <RootTabScreen
-              name="memories"
-              component={Memories}
-              options={{
-                title: "Your Memories",
-                tabBarIcon: ({ color, size }) => (
-                  <Fi name="book-open" size={size} color={color} />
-                ),
-                ...stdProps,
-              }}
-            /> */}
-              <RootTabScreen
-                name="settings"
-                component={Settings}
-                options={{
-                  title: "User Settings",
-                  tabBarIcon: ({ color, size }) => (
-                    <Fi name="settings" size={size} color={color} />
-                  ),
-                  ...stdProps,
-                }}
-              />
-            </RootTabNavigator>
-          </NavigationContainer>
+          <Layout />
         </AuthProvider>
       </QueryClientProvider>
     </SafeAreaProvider>
---
+++ Layout.tsx
@@ -0,0 +1,91 @@
+import { BottomTabNavigationOptions } from "@react-navigation/bottom-tabs";
+import React from "react";
+import { useAuth } from "./contexts/auth";
+import { RootTabNavigator, RootTabScreen } from "./navigation";
+import { Shows, Scan, Showings, Settings } from "./screens";
+import { NavigationContainer } from "@react-navigation/native";
+import Fi from "react-native-vector-icons/Feather";
+
+const stdProps: BottomTabNavigationOptions = {
+  tabBarShowLabel: false,
+};
+
+export default function Layout() {
+  const { user } = useAuth();
+
+  return (
+    <NavigationContainer>
+      <RootTabNavigator initialRouteName="scan">
+        {/* <RootTabScreen
+              name="home"
+              component={Home}
+              options={{
+                title: "Home",
+                tabBarIcon: ({ color, size }) => (
+                  <Fi name="home" size={size} color={color} />
+                ),
+                ...stdProps,
+              }}
+            /> */}
+        <RootTabScreen
+          name="shows"
+          component={Shows}
+          options={{
+            title: "Shows",
+            tabBarIcon: ({ color, size }) => (
+              <Fi name="info" size={size} color={color} />
+            ),
+            ...stdProps,
+          }}
+        />
+        <RootTabScreen
+          name="scan"
+          component={Scan}
+          options={{
+            title: "Scan Ticket",
+            unmountOnBlur: true, // unmount camera when not in view
+            tabBarIcon: ({ color, size }) => (
+              <Fi name="camera" size={size} color={color} />
+            ),
+            ...stdProps,
+          }}
+        />
+        {/* <RootTabScreen
+              name="memories"
+              component={Memories}
+              options={{
+                title: "Your Memories",
+                tabBarIcon: ({ color, size }) => (
+                  <Fi name="cloud" size={size} color={color} />
+                ),
+                ...stdProps,
+              }}
+            /> */}
+        {user && (
+          <RootTabScreen
+            name="showings"
+            component={Showings}
+            options={{
+              title: "Showings",
+              tabBarIcon: ({ color, size }) => (
+                <Fi name="book" size={size} color={color} />
+              ),
+              ...stdProps,
+            }}
+          />
+        )}
+        <RootTabScreen
+          name="settings"
+          component={Settings}
+          options={{
+            title: "User Settings",
+            tabBarIcon: ({ color, size }) => (
+              <Fi name="settings" size={size} color={color} />
+            ),
+            ...stdProps,
+          }}
+        />
+      </RootTabNavigator>
+    </NavigationContainer>
+  );
+}
