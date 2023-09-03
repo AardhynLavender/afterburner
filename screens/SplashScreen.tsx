7e00bd371034625974aaa6c8338736ae2b9acd75
Aardhynlavneder <laveat1@student.op.ac.nz>
2023-09-03 16:13:36 +1200

    chore: try animated view without reanimated

--- screens/SplashScreen.tsx
+++ screens/SplashScreen.tsx
@@ -1,57 +1,57 @@
 import React, { useEffect, useRef } from "react";
-import { View, Text, StyleSheet, /*Animated,*/ Easing } from "react-native";
+import { View, Text, StyleSheet, Animated, Easing } from "react-native";
 
 export default function SplashScreen({ text }: { text?: string }) {
-  // const angle = useSpin();
+  const angle = useSpin();
 
   return (
     <View style={styles.container}>
-      {/* <Animated.View
+      <Animated.View
         style={{
           ...styles.spinner,
           transform: [{ rotate: angle }],
         }}
-      /> */}
+      />
       {text && <Text style={styles.text}>{text}</Text>}
     </View>
   );
 }
 
-// const MIN_ANGLE = 0;
-// const MAX_ANGLE = 360;
-// const INITIAL_ANGLE = 0;
-// const DEFAULT_DURATION = 1000;
-// const UNIT = 1;
-// function useSpin({
-//   minAngle = MIN_ANGLE,
-//   maxAngle = MAX_ANGLE,
-//   initialAngle = INITIAL_ANGLE,
-//   duration = DEFAULT_DURATION,
-// }: {
-//   minAngle?: number;
-//   maxAngle?: number;
-//   initialAngle?: number;
-//   duration?: number;
-// } = {}) {
-//   const unit = useRef(new Animated.Value(initialAngle));
-//   useEffect(() => {
-//     Animated.loop(
-//       Animated.timing(unit.current, {
-//         toValue: UNIT,
-//         duration,
-//         easing: Easing.linear,
-//         useNativeDriver: true,
-//       })
-//     ).start();
-//   }, [duration]);
+const MIN_ANGLE = 0;
+const MAX_ANGLE = 360;
+const INITIAL_ANGLE = 0;
+const DEFAULT_DURATION = 1000;
+const UNIT = 1;
+function useSpin({
+  minAngle = MIN_ANGLE,
+  maxAngle = MAX_ANGLE,
+  initialAngle = INITIAL_ANGLE,
+  duration = DEFAULT_DURATION,
+}: {
+  minAngle?: number;
+  maxAngle?: number;
+  initialAngle?: number;
+  duration?: number;
+} = {}) {
+  const unit = useRef(new Animated.Value(initialAngle));
+  useEffect(() => {
+    Animated.loop(
+      Animated.timing(unit.current, {
+        toValue: UNIT,
+        duration,
+        easing: Easing.linear,
+        useNativeDriver: true,
+      })
+    ).start();
+  }, [duration]);
 
-//   const angle = unit.current.interpolate({
-//     inputRange: [0, UNIT],
-//     outputRange: [`${minAngle}deg`, `${maxAngle}deg`],
-//   });
+  const angle = unit.current.interpolate({
+    inputRange: [0, UNIT],
+    outputRange: [`${minAngle}deg`, `${maxAngle}deg`],
+  });
 
-//   return angle;
-// }
+  return angle;
+}
 
 const SPINNER_SIZE = 64;
 const SPINNER_BAND_WIDTH = 4;
